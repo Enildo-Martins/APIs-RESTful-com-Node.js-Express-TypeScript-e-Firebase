f07db2ce1487a057e5153e2716928883e344f8b6
Enildo-Martins <enildooliveira1@gmail.com>
2025-05-27 16:31:23 -0300

    #3 feat: implementando alguns metodos HTTP

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req, res) =>{
-    res.send("Hello tsc-watch");
+app.use(express.json());
 
-})
+app.get("/", (req: Request, res: Response) =>{
+    res.send("Hello tsc-watch");
+});
+
+let usuarios = [{
+    nome: "Enildo Martins",
+    idade: 20
+}, {
+    nome: "João da Silva",
+    idade: 33
+}];
+
+
+app.get("/users", (req: Request, res: Response) =>{
+    res.send(usuarios);
+});
+
+app.post("/users", (req: Request, res: Response) =>{
+    let user = req.body;
+    usuarios.push(user);
+    res.send({
+        message: "Usuario criado com sucesso!"
+    });
+});
 
 app.listen(3000, () => {
     console.log("Servidor ativo na porta 3000");
 
-})
+});
