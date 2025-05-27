2a655997b498dbdea5c4d83a9fc0e9f33a842c74
Enildo-Martins <enildooliveira1@gmail.com>
2025-05-27 17:39:39 -0300

    #3 feat: implementado as rotas PUT e DELETE

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,13 @@ app.get("/", (req: Request, res: Response) =>{
     res.send("Hello tsc-watch");
 });
 
+type User =  {
+    id: number; 
+    nome: string; 
+    email: string;
+};
 let id = 0;
-let usuarios: {id: number, nome: string, email: string}[] = [];      
+let usuarios: User[] = [];      
 
 app.get("/users", (req: Request, res: Response) => {
     res.send(usuarios);
@@ -28,6 +33,26 @@ app.post("/users", (req: Request, res: Response) =>{
     res.send({
         message: "Usuario criado com sucesso!"
     });
+});
+
+app.put("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let user = req.body;
+    let indexOf = usuarios.findIndex((_user: User) => _user.id === userId);
+    usuarios[indexOf].nome = user.nome;
+    usuarios[indexOf].email = user.email;
+    res.send({
+        message: "Usuario alterado com sucesso!"
+    });
+});
+
+app.delete("/users/:id", (req: Request, res: Response) => {
+    let userId = Number(req.params.id);
+    let indexOf = usuarios.findIndex((user: User) => user.id === userId);
+    usuarios.splice(indexOf, 1);
+    res.send({
+        message: "Usuario excluido com sucesso!"
+    })
 });
 
 app.listen(3000, () => {
