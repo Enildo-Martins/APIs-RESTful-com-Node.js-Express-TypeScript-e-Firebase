1691e8df995d00556bbb8fd30ce7f162baacf871
Enildo-Martins <enildooliveira1@gmail.com>
2025-05-29 16:24:36 -0300

    #5 feat: conectando a API ao Firebase Admin SDK

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import express from "express";
+import * as admin from "firebase-admin";
 import {routes} from "./routes/index";
+
+const serviceAccount = require('../firebase-adminsdk.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 const app = express();
 
@@ -7,5 +14,4 @@ routes(app);
 
 app.listen(3000, () => {
     console.log("Servidor ativo na porta 3000");
-
 });
